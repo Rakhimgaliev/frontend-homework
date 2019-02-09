3a5b9bb249c4771945c21b4b53dce0056eecccdd
Temirlan <rakhimgaliev56@gmail.com>
2019-02-09 22:30:33 +0300

    done

---
+++ source/letters.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const letters = function DoIt(str, flag) {
+    var res = "";
+    if (flag === undefined) {
+        for (var i = 0; i < str.length; i++) {
+            if (str.lastIndexOf(str[i]) == str.indexOf(str[i])) {
+                res += str[i];
+            }
+        }
+    } else if (flag === true) {
+        for (var i = 0; i < str.length; i++) {
+            if (i == str.indexOf(str[i])) {
+                res += str[i];
+            }
+        }
+    } else if (flag === false) {
+        for (var i = 0; i < str.length; i++) {
+            if (i == str.lastIndexOf(str[i])) {
+                res += str[i];
+            }
+        }
+    }
+    return res;
+}
